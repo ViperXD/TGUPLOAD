0b8e6edf15fbfa5864e14b56087121cb91287c3b
Hirusha-H <77770753+Hirusha-H@users.noreply.github.com>
2021-03-13 18:58:42 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# @NexaBotsUpdates
-
-class Config(object):
-
-    BOT_TOKEN = "1695608546:AAHaCoq5KUz7WmdyszacidBWSBfgoDSxpEM"
-
-
-    APP_ID = 2294033
-    API_HASH = "6a8fba10d5e2747b3c101c03d2d22752"
